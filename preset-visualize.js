7b7d48ed69a8d244505ce3c94da476a7171752f3
D050079 <mi.graf@sap.com>
2024-11-29 15:26:31 +0100

    * add busy indication for loading insights * replace button with the insights instead of separate div

--- preset-visualize.js
+++ preset-visualize.js
@@ -545,6 +545,14 @@ function buildInsightsBtn(container, data) {
     insightsBtn.innerText = 'Generate Insights';
     insightsBtn.classList.add('border', 'rounded', 'bg-blue-500', 'text-white', 'p-2', 'my-2');
     insightsBtn.onclick = async () => {
+        // busy
+
+        insightsBtn.innerText = '';
+        insightsBtn.insertAdjacentHTML('beforeend', '<svg class="animate-spin mt-0.5 mr-3 h-5 w-5 text-white" style="float: left" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">\n' +
+            '      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>\n' +
+            '      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>\n' +
+            '    </svg>Loading...');
+
         const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${geminiApiKey}`, {
             method: 'POST',
             headers: {
@@ -563,7 +571,8 @@ function buildInsightsBtn(container, data) {
 
         if (response.status === 200) {
             const insights = responseBody.candidates[0].content.parts[0].text;
-            const insightsDiv = document.createElement('div');
+            insightsDiv.removeChild(insightsBtn);
+            insightsDiv.classList.remove("text-center");
             insightsDiv.classList.add('markdown-content', 'border', 'rounded', 'p-4', 'my-4', 'bg-blue-100');
 
             // Convert Markdown to HTML and set it as the inner HTML of the div
